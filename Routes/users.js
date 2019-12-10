b811b61a25911d6743aa7fb585d4f2f61578eceb
Aleksandar <asimatovic@sogyo.nl>
2019-12-10 13:24:46 +0100

    Add login and register backend

--- Routes/users.js
+++ Routes/users.js
@@ -1,64 +1,123 @@
 const router = require('express').Router();
+const cors = require('cors');
 const jwt = require('jsonwebtoken');
-const secret = process.env.SECRET;
+const bcrypt = require('bcrypt');
 let User = require('../models/User');
+router.use(cors())
 
-
+const secret = process.env.SECRET;
 
 router.route('/api/register').post((req,res) => {
 
+    const username = req.body.username;
     const email = req.body.email;
     const password = req.body.password;
+    const today = new Date();
+
     const newUser = new User({
+        today,
+        username,
         email,
         password
     });
 
-    newUser.save()
-    .then(() => res.json("Registered!"))
-    .catch(err => res.status(400).json("Error: " + err));
-
-});
-
-router.route('/api/authenticate').post((req, res) => {
-    const email = req.body.email;
-      const password = req.body.password;
-      User.findOne({ email }, function(err, user) {
-            if (err) {
-                  console.error(err);
-                  res.status(500)
-                    .json({
-                    error: 'Internal error please try again'
-                  });
-                } else if (!user) {
-                  res.status(401)
-                    .json({
-                      error: 'Incorrect email or password'
-                    });
-                } else {
-                  user.isCorrectPassword(password, function(err, same) {
-                        if (err) {
-                              res.status(500)
-                                .json({
-                                  error: 'Internal error please try again'
-                              });
-                            } else if (!same) {
-                              res.status(401)
-                                .json({
-                                  error: 'Incorrect email or password'
-                              });
-                            } else {
-                              // Issue token
-                              const payload = { email };
-                              const token = jwt.sign(payload, secret, {
-                                expiresIn: '1h'
-                              });
-                              res.cookie('token', token, { httpOnly: true })
-                                .sendStatus(200);
-                        }
-              });
+    User.findOne({
+        email: req.body.email
+    })
+    .then(user => {
+        if(!user) {
+            bcrypt.hash(req.body.password, 10, (err, hash) => {
+                newUser.password = hash
+                User.create(newUser)
+                .then(user => {
+                    res.json({status: user.email + "Registered"})
+                })
+                .catch(err => {
+                    res.send('error: ' + err)
+                })
+            })
         }
-      });
+        else{
+            res.json({ error: "Email already exists" })
+        }
+    })
+    .catch(err => {
+        res.send('error: ' + err)
+    })
+
 });
+
+router.route('/api/login').post((req,res) => {
+
+    User.findOne({
+        email: req.body.email
+    })
+    .then(user => {
+        if(user) {
+            if(bcrypt.compareSync(req.body.password, user.password))  {
+                const payload = {
+                    _id: user._id,
+                    username: user.username,
+                    email: user.email
+                }
+                let token = jwt.sign(payload, process.env.SECRET, {
+                    expiresIn: 1440
+                })
+                res.send(token)
+            }
+            else{
+                res.json({error: "Email and password don't match"})
+            }
+        }
+        else{
+            res.json({ error: "User doesn't exists" })
+        }
+    })
+    .catch(err => {
+        res.send('error: ' + err)
+    })
+
+});
+
+//router.route('/api/authenticate').post((req, res) => {
+//    const email = req.body.email;
+//      const password = req.body.password;
+//      User.findOne({ email }, function(err, user) {
+//            if (err) {
+//                  console.error(err);
+//                  res.status(500)
+//                    .json({
+//                    error: 'Internal error please try again'
+//                  });
+//                } else if (!user) {
+//                  res.status(401)
+//                    .json({
+//                      error: 'Incorrect email or password'
+//                    });
+//                } else {
+//                  user.isCorrectPassword(password, function(err, same) {
+//                        if (err) {
+//                              res.status(500)
+//                                .json({
+//                                  error: 'Internal error please try again'
+//                              });
+//                            } else if (!same) {
+//                              res.status(401)
+//                                .json({
+//                                  error: 'Incorrect email or password'
+//                              });
+//                            } else {
+//                              // Issue token
+//                              const payload = { email };
+//                              const token = jwt.sign(payload, secret, {
+//                                expiresIn: '1h'
+//                              });
+//                              res.cookie('token', token, { httpOnly: true })
+//                                .sendStatus(200);
+//                        }
+//              });
+//        }
+//      });
+//});
 
 module.exports = router;
--- models/User.js
+++ models/User.js
@@ -1,40 +1,64 @@
-const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
-
-const saltRounds = 10;
-
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+
 const UserSchema = new Schema({
-    email: {type: String, unique: true},
-    password: {type: String}
+  username: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
 });
 
-UserSchema.pre('save', function(next) {
-    if(this.isNew) {
-        const document = this;
-        bcrypt.hash(document.password, saltRounds,
-        (err, hashedPassword) => {
-            if(err){
-                next(err);
-            }
-            else{
-                document.password = hashedPassword;
-                next();
-            }
-        });
-        this.password = "dsgdfsgfdsgsdfg";
-    }
-    else {
-        this.password = "headgf";
-        next();
-    }
-});
+module.exports = User = mongoose.model("users", UserSchema);
 
-UserSchema.methods.isCorrectPassword = function(password, callback){
-    bcrypt.compare(password, this.password, function(err, same){
-       if(err) callback(err);
-       else callback(err, same);
-    });
-}
-
-module.exports = mongoose.model('User', UserSchema);
+//const mongoose = require('mongoose');
+//const bcrypt = require('bcrypt');
+//
+//const saltRounds = 10;
+//
+//const Schema = mongoose.Schema;
+//const UserSchema = new Schema({
+//    email: {type: String, unique: true},
+//    password: {type: String}
+//});
+//
+//UserSchema.pre('save', function(next) {
+//    if(this.isNew) {
+//        const document = this;
+//        bcrypt.hash(document.password, saltRounds,
+//        (err, hashedPassword) => {
+//            if(err){
+//                next(err);
+//            }
+//            else{
+//                document.password = hashedPassword;
+//                next();
+//            }
+//        });
+//        this.password = "dsgdfsgfdsgsdfg";
+//    }
+//    else {
+//        this.password = "headgf";
+//        next();
+//    }
+//});
+//
+//UserSchema.methods.isCorrectPassword = function(password, callback){
+//    bcrypt.compare(password, this.password, function(err, same){
+//       if(err) callback(err);
+//       else callback(err, same);
+//    });
+//}
+//
+//module.exports = mongoose.model('User', UserSchema);
