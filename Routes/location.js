dab80f5dc1c86d2c4e480622fd71fd2d67bf2d27
Aleksandar <asimatovic@sogyo.nl>
2019-12-12 13:26:17 +0100

    Move getCityInfo fetch from client to server

--- Routes/location.js
+++ Routes/location.js
@@ -4,7 +4,6 @@ var fetch = require('node-fetch')
 
 router.route('/getCurrentLocation').post((req, res) => {
     const currentLocation = req.body.cityName;
-    console.log(currentLocation)
     const url = "http://api.geonames.org/searchJSON?name_equals=" + currentLocation + "&username=destinationFinder";
     fetch(url)
     .then(res => res.json())
@@ -14,6 +13,16 @@ router.route('/getCurrentLocation').post((req, res) => {
     .catch(console.log);
 });
 
+router.route('/getCityInfo').post((req, res) => {
+    const city = req.body.cityName;
+    const url = "http://api.geonames.org/searchJSON?name_equals=" + city + "&username=destinationFinder";
+    fetch(url)
+    .then(res => res.json())
+    .then(data => {
+        res.json(data);
+    })
+    .catch(console.log);
+});
 
 
 module.exports = router;
