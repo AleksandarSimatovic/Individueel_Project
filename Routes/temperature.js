e2c223a167daeb51f0065550e46f96204103276f
Aleksandar <asimatovic@sogyo.nl>
2019-12-12 12:05:01 +0100

    Move getTemp fetch from client to server

---
+++ Routes/temperature.js
@@ -0,0 +1,19 @@
+const router = require('express').Router();
+const express = require('express');
+var fetch = require('node-fetch')
+
+
+router.route('/getTemperature').post((req, res) => {
+
+    const cityName = req.body.cityName;
+    const url = "http://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&APPID=840e44ccd3376f040093e83e5be8f34f";
+    fetch(url)
+    .then(res => res.json())
+    .then(data => {
+        const temp = Number(data.main.temp) - 273.15;
+        res.json(temp);
+    })
+    .catch(console.log);
+   });
+
+module.exports = router;
