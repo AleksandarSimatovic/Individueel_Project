1f622e1def945cb083e215b4f5f70c910bb6015d
A <asimatovic@sogyo.nl>
2020-03-09 11:24:28 +0100

    Remove redundant

--- test/client/src/utilTest.js
+++
@@ -1,9 +0,0 @@
-const assert = require('chai').assert;
-const calculateDistance = require('../../../client/src/util').calculateDistance;
-
-describe('App', () => {
-    it('app should return 0', () => {
-       let result = calculateDistance(5,10,5,10);
-       assert.equal(result, 0);
-    });
-})
